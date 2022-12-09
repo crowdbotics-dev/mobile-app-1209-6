a423e11235c9cfa7a0dabc063466208559d549c5
Aline Tavares <aline@crowdbotics.com>
2022-12-09 12:12:54 -0500

    Crowdbotics Studio: Update app screens

--- screens/BlankScreen1/index.js
+++ screens/BlankScreen1/index.js
@@ -1,19 +1,25 @@
-
 import React from "react";
 import { View, StyleSheet } from "react-native";
 
 const BlankScreen1 = () => {
   return <View style={_styles.BlankScreen1}>
-      </View>;
+      <View style={_styles.ehZMmPYH}></View></View>;
 };
 
 export default BlankScreen1;
 
 const _styles = StyleSheet.create({
   BlankScreen1: {
-    backgroundColor: "#f0f0f1",
+    backgroundColor: "#ffffff",
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  ehZMmPYH: {
+    height: 111,
+    width: 336,
+    backgroundColor: "#fdd4e6",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
